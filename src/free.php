461655b2de1da781ea3614015fa0064deb62e2c4
Tobias Wooldridge <tobias@wooldridge.id.au>
2013-04-10 15:11:37 +0930

    List of all room bookings is now dumped to JSON

--- src/free.php
+++ src/free.php
@@ -17,6 +17,19 @@ function format_stusys_date(\DateTime $date) {
 	return strtoupper($date->format('d-M-Y'));
 }
 
+function get_inner_html( $node ) {
+    $innerHTML= '';
+    $children = $node->childNodes;
+    foreach ($children as $child) {
+        $innerHTML .= $child->ownerDocument->saveXML( $child );
+    }
+
+	// $innerHTML = preg_replace( "{[ \t]+}", ' ', $innerHTML );
+	// $innerHTML = str_replace( "\A0", ' ', $innerHTML );
+	$innerHTML = preg_replace('/\xA0/u', ' ', $innerHTML);
+
+    return $innerHTML;
+} 
 
 $date_format = '';
 $base_url = 'http://stusyswww.flinders.edu.au/roombook.taf';
@@ -48,7 +61,8 @@ $columns = $headerRow->children();
 $times = array();
 
 foreach ($columns as $column) {
-	$times[] = trim($column->textContent);
+	// <a href="/?(roombook|topic)\.taf\?(.*)">(<i>)?(.*)(</i>)?</a> (.*)<br/>
+	$times[] = trim($column->textContent, " \t\n\r\0\x0B\xC2\xA0");
 }
 array_shift($times);
 
@@ -67,21 +81,45 @@ foreach ($rows as $i => $row) {
 			continue;
 		}
 
-    	$room->bookings[$i - 3] = trim($column->textContent, " \t\n\r\0\x0B\xC2\xA0");
+		$pattern = '$<a href="/?(?P<type>roombook|topic)\.taf\?(?P<htmlQuery>.*?)">(<i>)?(?P<id>[\/A-z0-9]+)(</i>)?</a> (?P<name>[A-z0-9 \(\)]+)<br/>$';
+
+		$bookingHtml = get_inner_html($column);
+		if (preg_match_all($pattern, $bookingHtml, $matches, PREG_SET_ORDER) == 0) {
+			continue;
+		}
+
+		$bookingsData = array();
+		foreach ($matches as $match) {
+			$bookingData = array();
+
+			$bookingData['time'] = $times[$i - 3];
+
+			$bookingData['id'] = $match['id'];
+			$bookingData['name'] = $match['name'];
+			$htmlQuery = htmlspecialchars_decode($match['htmlQuery']);
+			parse_str($htmlQuery, $bookingData['queryString']);
+
+			$bookingData['type'] = $match['type'];
+
+			$bookingsData[] = $bookingData;
+
+		}
+
+    	$room->bookings[$i - 3] = $bookingData;
 	}
 	
 	$rooms[] = $room;
 }
 
-// var_dump($times);
 // var_dump($rooms);
+echo json_encode($rooms);
 
-foreach ($times as $i => $time) {
-	echo "Rooms free at at $time\n";
-	foreach($rooms as $room) {
-
-		if ($room->bookings[$i] == "") {
-			echo $room->id  . "\t" . $room->name . "\t"  . $room->bookings[$i] . "\n";
-		}
-	}
-}
+//foreach ($times as $i => $time) {
+//	echo "Rooms free at at $time\n";
+//	foreach($rooms as $room) {
+//
+//		if ($room->bookings[$i] == "") {
+//			echo $room->id  . "\t" . $room->name . "\t"  . $room->bookings[$i] . "\n";
+//		}
+//	}
+//}
